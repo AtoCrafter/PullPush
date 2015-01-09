e5e96461816fb60fcfff1b1978b7ce4bb80422a5
AtoCrafter <ato.crafter@gmail.com>
2015-01-09 18:24:18 +0900

    Extract BlockPullPush

--- src/main/scala/ato/pullpush/block/BlockPulling.scala
+++ src/main/scala/ato/pullpush/block/BlockPulling.scala
@@ -1,34 +1,13 @@
 package ato.pullpush.block
 
 import ato.pullpush.tileentity.TileEntityPulling
-import net.minecraft.block.material.Material
-import net.minecraft.block.{Block, ITileEntityProvider}
-import net.minecraft.creativetab.CreativeTabs
-import net.minecraft.entity.EntityLivingBase
-import net.minecraft.item.ItemStack
 import net.minecraft.tileentity.TileEntity
-import net.minecraft.util.MathHelper
 import net.minecraft.world.World
 
-class BlockPulling extends Block(Material.iron) with ITileEntityProvider {
-
+class BlockPulling extends BlockPullPush {
   setBlockName("PullBlock")
-  setCreativeTab(CreativeTabs.tabBlock)
+  setBlockTextureName("obsidian")
 
   override def createNewTileEntity(world: World, meta: Int): TileEntity =
     new TileEntityPulling()
-
-  override def onBlockPlacedBy(world: World, x: Int, y: Int, z: Int, entity: EntityLivingBase, itemstack: ItemStack) {
-    val meta = entity.rotationPitch match {
-      case p if p > 60 => 1
-      case p if p < -60 => 0
-      case _ => MathHelper.floor_double((entity.rotationYaw * 4 / 360) + 0.5) & 3 match {
-        case 0 => 2
-        case 1 => 5
-        case 2 => 3
-        case 3 => 4
-      }
-    }
-    world.setBlockMetadataWithNotify(x, y, z, meta, 2)
-  }
 }
